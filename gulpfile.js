350ed0f2ec66359af15403e046605e6cee1f9a06
oco-cho <77410049+oco-cho@users.noreply.github.com>
2021-03-11 18:13:38 -0800

    Final version (!!)

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-//The dependencies
+/********* THE DEPENDENCIES *********/
 var {src, dest, watch} = require('gulp');
 var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-//Task
+/********* TASK *********/
 function css () {
     return src('src/*.scss')
     .pipe(sass())
     .pipe(dest('dist/css'))
 };
 
-//Watch Function
+/********* WATCH *********/
 exports.default = function () {
     watch('src/*.scss', css);
     watch('src/modules/*.scss', css);
 };
 
-//Default Task
+/********* DEFAULT TASK *********/
 //exports.default = css;
